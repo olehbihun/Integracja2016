b0d7d23c1030a415a55211135106b8e607a8f4f4
olehbihun <olehbihun@gmail.com>
2016-12-12 22:05:30 +0100

    new
    new version

--- elements/xml/get_data.php
+++ elements/xml/get_data.php
@@ -15,13 +15,14 @@ if($res[0]['count']<300){
 
   $obj_db->callProc(" log('".$_SERVER['REMOTE_ADDR']."','".date('Y-m-d H:i:s')."','".' LANGUAGE:'.$_SERVER['HTTP_ACCEPT_LANGUAGE'].' HTTP_USER_AGENT: '.$_SERVER['HTTP_USER_AGENT']."')");
   $result = $obj_db->query("CALL ".$_POST['procedura']);
+  $file_name = rand(999999, 7777777777);
 
   switch ($_POST['format']) {
     case 'XML':
       $dom = new DOMDocument( "1.0");
       $dom->preserveWhiteSpace = false;
       $dom->formatOutput = true;
-      $xslt = $dom->createProcessingInstruction('xml-stylesheet', 'type="text/css" href="http://integracja.u464.indigo.elastictech.org/css/'.$_POST['style'].'.css"');
+      $xslt = $dom->createProcessingInstruction('xml-stylesheet', 'type="text/css" href="http://integracja.u464.indigo.elastictech.org/elements/xml/files/'.$file_name.'_'.$_POST['style'].'.css"');
       $dom->appendChild($xslt);
       $smartphones = $dom->appendChild($dom->createElement('smartphones'));
 
@@ -50,8 +51,32 @@ if($res[0]['count']<300){
       }
       $dom->formatOutput = true;
       $test1 = $dom->saveXML();
-      $name_xml = rand(999999, 7777777777).'.xml';
+      $name_xml = $file_name.'.xml';
       $dom->save('files/'.$name_xml);
+
+
+      $name_css = $file_name.'_'.$_POST['style'].'.css';
+      $fp = fopen('files/'.$name_css, 'w');
+
+      $css ='';
+      switch ($_POST['style']) {
+        case 'styles1':
+          $css = "@import url(https://fonts.googleapis.com/css?family=Aref+Ruqaa|Montserrat|Nova+Script|Oswald) smartphones{ font-family:".$_POST['fonts'].";  padding:".$_POST['padding_top']." 5% ".$_POST['padding_bottom']."!important; display:block;background:".$_POST['background_color'].";border-color:#3a173d}smartphone{  width:90%; margin:0 auto; display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .2s ease-out;-o-transition:all .2s ease-out;-webkit-transition:all .2s ease-out}camera,code,colour,dimensions,display,id,name,os,processor,ringtones,sim_standarts{font-size:".$_POST['font_size']." !important; color:".$_POST['color_fonts']."; text-align:center;width:10%;border:1px solid #cecbcb;word-wrap:break-word;border-left:0;padding:1px}smartphones smartphone:hover{background-color:". $_POST['color_hover'] ." !important; color:".$_POST['fonts_color_hover']."; cursor:pointer}smartphones smartphone:nth-child(odd){background-color:rgba(110,210,137,.68)}id{border-left:1px solid #cecbcb}";
+          //$css = "@import url(https://fonts.googleapis.com/css?family=Aref+Ruqaa|Montserrat|Nova+Script|Oswald);computers{font-family:".$_POST['fonts']."; padding:".$_POST['padding_top']." 5% ".$_POST['padding_bottom'].";display:block;background:".$_POST['background_color'].";border-color:#3a87ad}computer{display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .5s ease-out;-o-transition:all .5s ease-out;-webkit-transition:all .5s ease-out}HDD,cd_rom,code,color,cpu,data,id,link,name,opis,os,ram,screen,type_ram,weight{font-size:".$_POST['font_size']." !important; color:".$_POST['color_fonts']."; width:6%;border:1px solid ".$_POST['color_border'].";font-size:14px;word-wrap:break-word;border-left:0;padding:1px}computers computer:hover{background-color:". $_POST['color_hover'] ." !important;color:".$_POST['fonts_color_hover'].";cursor:pointer}computers computer:focus{background-color:red!important}computers computer:nth-child(1,3,8){background-color:rgba(110,210,137,.68)}id{word-wrap:normal;width:2%;border-left:1px solid #cecbcb}name{width:12%}code{width:5%}HDD,cd_rom,color,cpu,data,ink,os,screen,type_ram,weight{width:6%}ram{width:4%}opis{width:16%}";
+        break;
+        case 'styles2':
+          $css = "@import url(https://fonts.googleapis.com/css?family=Aref+Ruqaa|Montserrat|Nova+Script|Oswald) smartphones{ font-family:".$_POST['fonts2']."; padding:".$_POST['padding_top2']." 10px ".$_POST['padding_bottom2']."; display:block;background:".$_POST['background_color2'].";border-color:#3a173d}smartphone{  width:90%; margin:0 auto; display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .2s ease-out;-o-transition:all .2s ease-out;-webkit-transition:all .2s ease-out}camera,code,colour,dimensions,display,id,name,os,processor,ringtones,sim_standarts{font-size:".$_POST['font_size2']." !important; color:".$_POST['color_fonts2']."; text-align:center;width:10%;border:1px solid #cecbcb;word-wrap:break-word;border-left:0;padding:1px}smartphones smartphone:hover{background-color:". $_POST['color_hover2'] ." !important; color:".$_POST['fonts_color_hover2']."; cursor:pointer}smartphones smartphone:nth-child(odd){background-color:rgba(110,210,137,.68)}id{border-left:1px solid #cecbcb}";
+
+          //$css ="smartphones{padding:10px 5%;display:block;background:#a59ca9;border-color:#3a173d}smartphone{display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .2s ease-out;-o-transition:all .2s ease-out;-webkit-transition:all .2s ease-out}camera,code,colour,dimensions,display,id,name,os,processor,ringtones,sim_standarts{text-align:center;width:10%;border:1px solid #cecbcb;font-size:18px;word-wrap:break-word;border-left:0;padding:1px}smartphones smartphone:hover{background-color:#636382!important;color:#fffbfb;cursor:pointer}smartphones smartphone:nth-child(odd){background-color:rgba(110,210,137,.68)}id{border-left:1px solid #cecbcb}";
+//          $css = "@import url(https://fonts.googleapis.com/css?family=Oswald);computers{padding:5%;display:block;background:#fff;border-color:#3a87ad;font-family:Oswald,sans-serif}computer{display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .5s ease-out;-o-transition:all .5s ease-out;-webkit-transition:all .5s ease-out}HDD,cd_rom,code,color,cpu,data,id,link,name,opis,os,ram,screen,type_ram,weight{width:6%;border:1px solid #cecbcb;font-size:14px;word-wrap:break-word;border-left:0;padding:1px;text-align:center}computers computer:hover{background-color:#577170!important;color:#fffbfb;cursor:pointer}computers computer:focus{background-color:red!important}computers computer:nth-child(even){background-color:rgba(110,210,137,.68)}id{word-wrap:normal;width:2%;border-left:1px solid #cecbcb}name{width:12%}code{width:5%}HDD,cd_rom,color,cpu,data,ink,os,screen,type_ram,weight{width:6%}ram{width:4%}opis{width:16%}";
+        break;
+        case 'styles3':
+          $css = "@import url(https://fonts.googleapis.com/css?family=Aref+Ruqaa|Montserrat|Nova+Script|Oswald) smartphones{ font-family:".$_POST['fonts3']."; padding:".$_POST['padding_top3']." 10% ".$_POST['padding_bottom3']."; display:block;background:".$_POST['background_color3'].";border-color:#3a173d}smartphone{  width:90%; margin:0 auto; display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .2s ease-out;-o-transition:all .2s ease-out;-webkit-transition:all .2s ease-out}camera,code,colour,dimensions,display,id,name,os,processor,ringtones,sim_standarts{font-size:".$_POST['font_size3']." !important; color:".$_POST['color_fonts3']."; text-align:center;width:10%;border:1px solid #cecbcb;word-wrap:break-word;border-left:0;padding:1px}smartphones smartphone:hover{background-color:". $_POST['color_hover3'] ." !important; color:".$_POST['fonts_color_hover3']."; cursor:pointer}smartphones smartphone:nth-child(odd){background-color:rgba(110,210,137,.68)}id{border-left:1px solid #cecbcb}";
+          //$css = "@import url(https://fonts.googleapis.com/css?family=Raleway);smartphones{padding:1% 5%;display:block;border-color:#3a87ad;font-family:Raleway,sans-serif}smartphone{display:flex;background:#f7f7f7;border-color:#b94a48;-moz-transition:all .1s ease-out;-o-transition:all .1s ease-out;-webkit-transition:all .1s ease-out}camera,code,colour,dimensions,display,id,name,os,processor,ringtones,sim_standarts{width:10%;border:1px solid #cecbcb;font-size:12px;word-wrap:break-word;border-left:0;padding:1px;text-align:center}smartphones smartphone:hover{background-color:rgba(173,57,57,.52)!important;color:#fffbfb;cursor:pointer}smartphones smartphone:nth-child(odd){background-color:rgba(141,210,137,.68)}id{word-wrap:normal;border-left:1px solid #cecbcb}";
+        break;
+      }
+      fwrite($fp, $css );
+      fclose($fp);
 
       break;
     case 'JSON':
--- index.php
+++ index.php
@@ -28,6 +28,372 @@
     <input type="radio" name="format" value="styles3" id="style2"> template 3
   </form>
 
+  <table>
+  <tbody>
+    <tr>
+      <td>styles 1</td>
+      <td>styles 2</td>
+      <td>styles 3</td>
+    </tr>
+    <tr>
+      <td>
+        <label for="fonts">font</label>
+        <select   name="fonts" id="fonts">
+          <option value="'Aref Ruqaa', serif">Aref Ruqaa</option>
+          <option value="'Oswald', sans-serif">Oswald</option>
+          <option selected  value="'Montserrat', sans-serif">Montserrat</option>
+          <option value="'Nova Script', cursive">Nova Script</option>
+        </select>
+        <br>
+
+        <label for="font-size">font size</label>
+        <select   name="font-size" id='font-size'>
+          <option value="10px">10px</option>
+          <option selected  value="15px">15px</option>
+          <option value="20px">20px</option>
+          <option value="25px">25px</option>
+        </select>
+        <br>
+
+        <label for="background-color">background color</label>
+        <select   name="background-color" id="background-color">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option selected  value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option selected value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option  value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+
+
+        </select>
+        <br>
+
+        <label for="color-fonts">color fonts</label>
+        <select   name="color-fonts" id="color-fonts">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option  value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option selected value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="color-hover">color hover</label>
+        <select   name="color-hover" id="color-hover">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option selected value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="color-border">color border</label>
+        <select   name="color-border" id="color-border">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option  value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option selected  value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="fonts-color-hover">background color hover</label>
+        <select   name="fonts-color-hover" id="fonts-color-hover">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option selected  value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option  value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+
+        <label for="background-elements">background elements</label>
+        <select   name="background-elements" id="background-elements">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option selected value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="padding-top">padding top</label>
+        <select   name="padding-top" id="padding-top">
+          <option value="5px">5px</option>
+          <option selected  value="10px">10px</option>
+          <option value="15px">15px</option>
+        </select>
+        <br>
+
+        <label for="padding-bottom">padding bottom</label>
+        <select   name="padding-bottom" id="padding-bottom">
+          <option value="5px">5px</option>
+          <option value="10px">10px</option>
+          <option value="15px">15px</option>
+        </select>
+        <br>
+      </td>
+      <!-- template 2 -->
+      <td>
+
+        <label for="fonts2">font</label>
+        <select   name="fonts2" id="fonts2">
+          <option value="'Aref Ruqaa', serif">Aref Ruqaa</option>
+          <option value="'Oswald', sans-serif">Oswald</option>
+          <option value="'Montserrat', sans-serif">Montserrat</option>
+          <option value="'Nova Script', cursive">Nova Script</option>
+        </select>
+        <br>
+
+        <label for="font-size2">font size</label>
+        <select   name="font-size2" id='font-size2'>
+          <option value="10px">10px</option>
+          <option selected   value="15px">15px</option>
+          <option value="20px">20px</option>
+          <option value="25px">25px</option>
+        </select>
+        <br>
+
+        <label for="background-color2">background color</label>
+        <select   name="background-color2" id="background-color2">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option selected value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+
+
+        </select>
+        <br>
+
+        <label for="color-fonts2">color fonts</label>
+        <select   name="color-fonts2" id="color-fonts2">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option selected value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="color-hover2">color hover</label>
+        <select   name="color-hover2" id="color-hover2">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option selected value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option  value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="color-border2">color border</label>
+        <select   name="color-border2" id="color-border2">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option selected value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="fonts-color-hover2">background color hover</label>
+        <select   name="fonts-color-hover2" id="fonts-color-hover2">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option selected value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+
+        <label for="background-elements2">background elements</label>
+        <select   name="background-elements2" id="background-elements2">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option selected value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="padding-top2">padding top</label>
+        <select   name="padding-top2" id="padding-top2">
+          <option value="5px">5px</option>
+          <option selected value="10px">10px</option>
+          <option value="15px">15px</option>
+        </select>
+        <br>
+
+        <label for="padding-bottom2">padding bottom</label>
+        <select   name="padding-bottom2" id="padding-bottom2">
+          <option value="5px">5px</option>
+          <option selected value="10px">10px</option>
+          <option value="15px">15px</option>
+        </select>
+        <br>
+
+      </td>
+      <!-- template 3 -->
+      <td>
+
+        <label for="fonts3">font</label>
+        <select   name="fonts3" id="fonts3">
+          <option value="'Aref Ruqaa', serif">Aref Ruqaa</option>
+          <option value="'Oswald', sans-serif">Oswald</option>
+          <option selected value="'Montserrat', sans-serif">Montserrat</option>
+          <option value="'Nova Script', cursive">Nova Script</option>
+        </select>
+        <br>
+
+        <label for="font-size3">font size</label>
+        <select   name="font-size3" id='font-size3'>
+          <option value="10px">10px</option>
+          <option value="15px">15px</option>
+          <option selected value="20px">20px</option>
+          <option value="25px">25px</option>
+        </select>
+        <br>
+
+        <label for="background-color3">background color</label>
+        <select   name="background-color3" id="background-color3">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option selected value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+
+
+        </select>
+        <br>
+
+        <label for="color-fonts3">color fonts</label>
+        <select   name="color-fonts3" id="color-fonts3">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option selected value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option selected value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="color-hover3">color hover</label>
+        <select   name="color-hover3" id="color-hover3">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option selected value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option selected value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="color-border3">color border</label>
+        <select   name="color-border3" id="color-border3">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option selected value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option selected value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="fonts-color-hover3">background color hover</label>
+        <select   name="fonts-color-hover3" id="fonts-color-hover3">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option value="#8A2BE2">BlueViolet </option>
+          <option selected value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option value="#FF7F50">Coral </option>
+          <option selected value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+
+        <label for="background-elements3">background elements</label>
+        <select   name="background-elements3" id="background-elements3">
+          <option value="#F0F8FF">AliceBlue</option>
+          <option value="#00FFFF">Aqua </option>
+          <option selected value="#8A2BE2">BlueViolet </option>
+          <option value="#A52A2A">Brown</option>
+          <option value="#7FFF00">Chartreuse </option>
+          <option selected value="#FF7F50">Coral </option>
+          <option value="#BDB76B">DarkKhaki </option>
+          <option value="#2F4F4F">DarkSlateGray </option>
+        </select>
+        <br>
+
+        <label for="padding-top3">padding top</label>
+        <select   name="padding-top3" id="padding-top3">
+          <option value="5px">5px</option>
+          <option value="10px">10px</option>
+          <option value="15px">15px</option>
+        </select>
+        <br>
+
+        <label for="padding-bottom3">padding bottom</label>
+        <select   name="padding-bottom3" id="padding-bottom3">
+          <option value="5px">5px</option>
+          <option selected value="10px">10px</option>
+          <option value="15px">15px</option>
+        </select>
+        <br>
+
+      </td>
+    </tr>
+  </tbody>
+  </table>
+
   <p>Pobramie elementow za pomoca roznych fukcji baz danych</p>
 
     <a class="first" href="#">pobiermaie pirwszych elementow </a><br>
@@ -111,8 +477,49 @@
               }
             }
 
+            var fonts = $('#fonts').val();
+            var background_color =$('#background-color').val();
+            var color_fonts =$('#color-fonts').val();
+            var font_size =$('#font-size').val();
+            var background_fonts =$('#background-fonts').val();
+            var color_hover =$('#color-hover').val();
+            var color_border =$('#color-border').val();
+            var background_color_hover =$('#background-color-hover').val();
+            var padding_bottom =$('#padding-bottom').val();
+            var padding_top =$('#padding-top').val();
+            var fonts_color_hover =$('#fonts-color-hover').val();
 
-            $.ajax({type:"POST", url: url, data: {procedura:procedura,style:style,format:format }, dataType:"json",
+            var fonts2 = $('#fonts2').val();
+            var background_color2 =$('#background-color2').val();
+            var color_fonts2 =$('#color-fonts2').val();
+            var font_size2 =$('#font-size2').val();
+            var background_fonts2 =$('#background-fonts2').val();
+            var color_hover2 =$('#color-hover2').val();
+            var color_border2 =$('#color-border').val();
+            var background_color_hover2 =$('#background-color-hover2').val();
+            var padding_bottom2 =$('#padding-bottom2').val();
+            var padding_top2 =$('#padding-top2').val();
+            var fonts_color_hover2 =$('#fonts-color-hover2').val();
+
+            var fonts3 = $('#fonts3').val();
+            var background_color3 =$('#background-color3').val();
+            var color_fonts3 =$('#color-fonts3').val();
+            var font_size3 =$('#font-size3').val();
+            var background_fonts3 =$('#background-fonts3').val();
+            var color_hover3 =$('#color-hover3').val();
+            var color_border3 =$('#color-border').val();
+            var background_color_hover3 =$('#background-color-hover3').val();
+            var padding_bottom3 =$('#padding-bottom3').val();
+            var padding_top3 =$('#padding-top3').val();
+            var fonts_color_hover3 =$('#fonts-color-hover3').val();
+
+            $.ajax({type:"POST", url: url, data: {
+              procedura:procedura,
+              style:style, format:format,
+              fonts:fonts, background_color:background_color, color_fonts:color_fonts, font_size:font_size, background_fonts:background_fonts,color_hover:color_hover, color_border:color_border, background_color_hover:background_color_hover, padding_bottom:padding_bottom, padding_top:padding_top, fonts_color_hover:fonts_color_hover,
+              fonts2:fonts2, background_color2:background_color2, color_fonts2:color_fonts2, font_size2:font_size2, background_fonts2:background_fonts2,color_hover2:color_hover2, color_border2:color_border2, background_color_hover2:background_color_hover2, padding_bottom2:padding_bottom2, padding_top2:padding_top2, fonts_color_hover2:fonts_color_hover2,
+              fonts3:fonts3, background_color3:background_color3, color_fonts3:color_fonts3, font_size3:font_size3, background_fonts3:background_fonts3,color_hover3:color_hover3, color_border3:color_border3, background_color_hover3:background_color_hover3, padding_bottom3:padding_bottom3, padding_top3:padding_top3, fonts_color_hover3:fonts_color_hover3
+            }, dataType:"json",
                 success: function(data){window.location = "elements/xml/files/"+data; },
                 error: function(){ alert('limit pobierania danych pszekrocono! ');  }
             });
